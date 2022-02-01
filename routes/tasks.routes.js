c8edf1cd79f15bf27e1eb3ceaaed9932ca19aa90
FilopateerRasmy <filopateer.rasmy@outlook.com>
2022-02-01 19:43:26 +0200

    merging hisham

--- routes/tasks.routes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const { updateTask } = require("../controllers/tasks.controllers");
-const router = express.Router();
-
-router.route("/:id").patch(updateTask);
-
-module.exports = router;
